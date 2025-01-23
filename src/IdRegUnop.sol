6b6f48fcfac263e556fa47cc05e609395e4dedcc
DevDacian <114125154+devdacian@users.noreply.github.com>
2025-01-23 15:10:37 +0900

    eliminate separate interface file, add openzeppelin & solady for future exercises

--- src/IdRegUnop.sol
+++ src/IdRegUnop.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import { IIdReg } from "./IIdReg.sol";
+interface IIdReg {
+    // errors
+    error NumIdsOwnersLengthMismatch(uint256 numIds, uint256 ownersLength);
+
+    // public API
+    //
+    // view functions
+    function nextId() external view returns(uint256);
+    function idToOwner(uint256 id) external view returns(address);
+    function getOwnersForIds(uint256[] calldata ids) external view returns(address[] memory);
+
+    // non-view functions which change state
+    function generateIds(uint256 numIds, address[] memory owners) external;
+    function resetId(uint256 id) external;
+}
 
 // unoptimized implementation
 contract IdRegUnop is IIdReg {
